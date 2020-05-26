06e383662519150ceb47db89bbdfa75bba9b4b81
govendan <govendan2004@gmail.com>
2020-05-26 15:55:53 +0530

    4th Modification

--- test1.c
+++ test1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {    
 
-    int number1 = 15, number2=92, sum;
+    int number1 = 15, number2=92,number3=154; sum;
     
   
     // calculating sum
-    sum = number1 + number2;      
+    sum = number1 + number2 + number3;      
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d %d = %d", number1, number2, number3 ,sum);
     return 0;
 }
